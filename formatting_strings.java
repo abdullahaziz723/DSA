76ccbe17acc36ccb9ddd12647f3c0d65fa8acea0
Abdullah <abdullahaziz723@gmail.com>
2024-10-03 17:55:36 +0530

    formattting

---
+++ formatting_strings.java
@@ -0,0 +1,9 @@
+public class formatting_strings {
+
+    public static void main(String[] args) {
+        float a = 12.1272f;
+     //   System.out.printf("this is formatted string =%.2f",a);   
+     System.out.printf( "pie value is :-%.3f", Math.PI);
+
+     }
+}
