c887db0f74f059e26706a3c093e13cc11a331542
Abdullah <abdullahaziz723@gmail.com>
2024-10-03 18:04:04 +0530

    strings

--- formatting_strings.java
+++ formatting_strings.java
@@ -3,7 +3,14 @@ public class formatting_strings {
     public static void main(String[] args) {
         float a = 12.1272f;
      //   System.out.printf("this is formatted string =%.2f",a);   
-     System.out.printf( "pie value is :-%.3f", Math.PI);
+    //  System.out.printf( "pie value is :-%.3f", Math.PI);
+
+     //strings properties
+     String  str = "a"+"b";
+     System.out.println(str);
+
+     System.out.println('a'+'b');
+     System.out.println( (char) ('a'+3));
 
      }
 }
