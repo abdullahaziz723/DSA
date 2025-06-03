c7e102c5ef02a586726dfbcd4014fc74233f6a34
abdullah <abdullahaziz723@gmail.com>
2025-06-03 14:46:12 +0530

    func

--- DSA_revision/var_args.java
+++ DSA_revision/var_args.java
@@ -1,3 +1,12 @@
 public class var_args {
-    
+    public static void main(String[] args) {
+        multiple(2, 01, 3, 4, 5, 6, 7, 8, 9);   
+    }
+
+    static void multiple(int a,int b, int ...c){
+        System.out.println("the value of a is " + a);
+        System.out.println("the value of b is " + b);
+        System.out.println("the value of c is  "+c);   
+    }
 }
+
