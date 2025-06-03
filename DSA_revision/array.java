dd3c6b7ff83a213048cdf09fd13f09ec9dbcd919
abdullah <abdullahaziz723@gmail.com>
2025-06-04 00:00:03 +0530

    array

--- DSA_revision/array.java
+++ DSA_revision/array.java
@@ -1,7 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class array {
     public static void main(String[] args) {
-        string []nums = new string[4];
-        System.out.println(nums[0]);
-    
+        String []nums = new String[4];
+        Scanner in = new Scanner(System.in);
+        // System.out.println(nums[0]);
+        for(int i =0;i<nums.length;i++){
+            nums[i]=in.next();      }
+            System.out.println(Arrays.toString(nums));
+
+
     }
 }
